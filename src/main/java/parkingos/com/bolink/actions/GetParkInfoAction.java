8f78f261f8152781c32f46821a71efc75b4cc911
gaozhenmin <15238399787@139.com>
2018-04-02 14:13:40 +0800

    数据中心接口bug修改

--- src/main/java/parkingos/com/bolink/actions/GetParkInfoAction.java
+++ src/main/java/parkingos/com/bolink/actions/GetParkInfoAction.java
@@ -1,6 +1,7 @@
 package parkingos.com.bolink.actions;
 
 import com.mongodb.util.Hash;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,24 +14,28 @@ import javax.servlet.http.HttpServletResponse;
 import java.util.HashMap;
 import java.util.List;
 
+
 @Controller
 @RequestMapping("/getparkinfo")
 public class GetParkInfoAction {
     @Autowired
     private GetParkInfoService getParkInfoService;
+    Logger logger = Logger.getLogger(GetParkInfoAction.class);
     @RequestMapping(value = "/bygroupid")
     public String getInfoById(HttpServletRequest request, HttpServletResponse resp){
         int groupid = RequestUtil.getInteger(request,"groupid",0);
-       String  ret=getParkInfoService.getInfo(groupid);
-
+        logger.debug("数据中心请求groupId:"+groupid);
+        String  ret=getParkInfoService.getInfo(groupid);
+        logger.debug("数据中心返回："+ret);
         StringUtils.ajaxOutput(resp,ret);
         return null;
     }
     @RequestMapping(value = "/bycomid")
     public String getInfoBycomId(HttpServletRequest request, HttpServletResponse resp){
         int comid = RequestUtil.getInteger(request,"comid",0);
-        String  ret=getParkInfoService.getInfoByComid(comid);
-
+        logger.debug("数据中心请求groupId:"+comid);
+        String ret =getParkInfoService.getInfoByComid(comid);
+        logger.debug("数据中心返回:"+ret);
         StringUtils.ajaxOutput(resp,ret);
         return null;
     }
