e29b01cd3d259a356378b678e78ed9dc669863c9
chenbw <724791965@qq.com>
2019-03-01 18:18:29 +0800

    云函数计费

---
+++ src/main/java/parkingos/com/bolink/actions/ChargingAction.java
@@ -0,0 +1,107 @@
+package parkingos.com.bolink.actions;
+
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import parkingos.com.bolink.models.ParkChargingTb;
+import parkingos.com.bolink.service.ChargingService;
+import parkingos.com.bolink.service.SupperSearchService;
+import parkingos.com.bolink.utils.RequestUtil;
+import parkingos.com.bolink.utils.StringUtils;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/charging")
+public class ChargingAction {
+
+    Logger logger = LoggerFactory.getLogger(ChargingAction.class);
+
+    @Autowired
+    private ChargingService chargingService;
+    @Autowired
+    private SupperSearchService supperSearchService;
+
+    @RequestMapping("add")
+    public String add(HttpServletRequest request, HttpServletResponse resp){
+        String name = RequestUtil.getString(request,"name");
+        String remark = RequestUtil.getString(request,"remark");
+        String js = RequestUtil.getString(request,"js_content");
+        Long comid = RequestUtil.getLong(request,"comid",-1L);
+        JSONObject result = chargingService.addJs(name,remark,js,comid);
+        StringUtils.ajaxOutput(resp,result.toString());
+        return null;
+    }
+
+    @RequestMapping("query")
+    public String query(HttpServletRequest request, HttpServletResponse resp){
+        Map<String, String> reqParameterMap = RequestUtil.readBodyFormRequset(request);
+        Long comid = RequestUtil.getLong(request,"comid",-1L);
+        ParkChargingTb parkChargingTb = new ParkChargingTb();
+        parkChargingTb.setParkId(comid);
+        parkChargingTb.setState(0);
+        JSONObject result = supperSearchService.supperSearch(parkChargingTb,reqParameterMap);
+        StringUtils.ajaxOutput(resp,result.toString());
+        return null;
+    }
+
+    @RequestMapping("edit")
+    public String edit(HttpServletRequest request, HttpServletResponse resp){
+
+        Long id = RequestUtil.getLong(request,"id",-1L);
+        String name = RequestUtil.getString(request,"name");
+        String remark = RequestUtil.getString(request,"remark");
+        String jsContent = RequestUtil.getString(request,"js_content");
+
+        JSONObject result = chargingService.editParkCharging(id,name,remark,jsContent);
+        StringUtils.ajaxOutput(resp,result.toString());
+        return null;
+    }
+
+    @RequestMapping("delete")
+    public String delete(HttpServletRequest request, HttpServletResponse resp){
+
+        Long id = RequestUtil.getLong(request,"id",-1L);
+
+        JSONObject result = chargingService.deleteParkCharging(id);
+        StringUtils.ajaxOutput(resp,result.toString());
+        return null;
+    }
+
+
+    @RequestMapping("testjs")
+    public String testJs(HttpServletRequest request, HttpServletResponse resp){
+        Long id = RequestUtil.getLong(request,"id",-1L);
+        Long createTime = RequestUtil.getLong(request,"create_time",-1L);
+        Long endTime = RequestUtil.getLong(request,"end_time",-1L);
+        String result = chargingService.testJs(id,createTime,endTime);
+        StringUtils.ajaxOutput(resp,result);
+        return null;
+    }
+
+
+    @RequestMapping("openorclose")
+    public String openOrClose(HttpServletRequest request, HttpServletResponse resp){
+        Long id = RequestUtil.getLong(request,"id",-1L);
+        Integer isOpen = RequestUtil.getInteger(request,"is_open",0);
+        JSONObject result = chargingService.openOrClose(id,isOpen);
+        StringUtils.ajaxOutput(resp,result.toString());
+        return null;
+    }
+
+    @RequestMapping("cloudfirst")
+    public String cloudFirst(HttpServletRequest request, HttpServletResponse resp){
+        Long comid = RequestUtil.getLong(request,"comid",-1L);
+        Integer cloudFirst = RequestUtil.getInteger(request,"cloud_first",0);
+        JSONObject result = chargingService.cloudFirst(comid,cloudFirst);
+        StringUtils.ajaxOutput(resp,result.toString());
+        return null;
+    }
+
+}
---
+++ src/main/java/parkingos/com/bolink/service/impl/ChargingServiceImpl.java
@@ -0,0 +1,148 @@
+package parkingos.com.bolink.service.impl;
+
+import com.alibaba.fastjson.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import parkingos.com.bolink.dao.spring.CommonDao;
+import parkingos.com.bolink.models.ParkChargingTb;
+import parkingos.com.bolink.service.ChargingService;
+import parkingos.com.bolink.service.redis.RedisService;
+import parkingos.com.bolink.utils.Check;
+
+import javax.script.Invocable;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+
+public class ChargingServiceImpl implements ChargingService {
+
+    Logger logger = LoggerFactory.getLogger(ChargingServiceImpl.class);
+    @Autowired
+    private CommonDao commonDao;
+
+
+    @Override
+    public JSONObject addJs(String name, String remark, String js,Long comid) {
+        JSONObject result = new JSONObject();
+        result.put("state",0);
+        result.put("msg","添加失败");
+
+        ParkChargingTb parkChargingTb = new ParkChargingTb();
+        parkChargingTb.setParkId(comid);
+        parkChargingTb.setState(0);
+
+        int count = commonDao.selectCountByConditions(parkChargingTb);
+        if(count>0){
+            result.put("msg","该车场已经存在价格函数!");
+            return result;
+        }
+
+        parkChargingTb.setJsContent(js);
+        parkChargingTb.setName(name);
+        parkChargingTb.setRemark(remark);
+
+        int insert = commonDao.insert(parkChargingTb);
+        if(insert==1){
+            result.put("state",1);
+            result.put("msg","添加成功");
+        }
+
+        return result;
+    }
+
+    @Override
+    public JSONObject editParkCharging(Long id, String name, String remark, String jsContent) {
+        JSONObject result = new JSONObject();
+        result.put("state",0);
+        result.put("msg","修改失败！");
+        ParkChargingTb parkChargingTb = new ParkChargingTb();
+        parkChargingTb.setId(id);
+        parkChargingTb.setRemark(remark);
+        parkChargingTb.setName(name);
+        parkChargingTb.setJsContent(jsContent);
+        int update = commonDao.updateByPrimaryKey(parkChargingTb);
+        if(update==1){
+            result.put("state",1);
+            result.put("msg","修改成功！");
+        }
+        return result;
+    }
+
+    @Override
+    public JSONObject deleteParkCharging(Long id) {
+        JSONObject result = new JSONObject();
+        result.put("state",0);
+        result.put("msg","删除失败！");
+        ParkChargingTb pa = new ParkChargingTb();
+        pa.setState(1);
+        pa.setId(id);
+        int delete = commonDao.updateByPrimaryKey(pa);
+        if(delete==1){
+            result.put("state",1);
+            result.put("msg","删除成功！");
+        }
+        return result;
+    }
+
+    @Override
+    public String testJs(Long id,Long createTime,Long endTime) {
+
+        ParkChargingTb parkChargingTb = new ParkChargingTb();
+        parkChargingTb.setId(id);
+        parkChargingTb = (ParkChargingTb)commonDao.selectObjectByConditions(parkChargingTb);
+        if(parkChargingTb==null){
+            return "0";
+        }
+        String jsContent = parkChargingTb.getJsContent();
+        if(Check.isEmpty(jsContent)){
+            return "0";
+        }
+        String result = "0";
+        try {
+            ScriptEngineManager manager = new ScriptEngineManager();
+            ScriptEngine engine = manager.getEngineByName("javascript");
+            engine.eval(jsContent);
+            // javax.script.Invocable 是一个可选的接口
+            // 检查脚本引擎是否被实现!
+            // 注意：JavaScript engine 实现了 Invocable 接口
+            Invocable inv = (Invocable) engine;
+            // 执行这个名字为 "hello"的全局的函数
+            result = (String)inv.invokeFunction("getPrice", createTime, endTime);
+            logger.info("===>>>>get result from js:"+result);
+            return result;
+        }catch (Exception e){
+            logger.error("===>>>>>js 解析异常",e);
+        }
+        return result;
+    }
+
+    @Override
+    public JSONObject openOrClose(Long id, Integer isOpen) {
+
+        JSONObject result = new JSONObject();
+        result.put("state",0);
+        result.put("msg","更新失败！");
+        ParkChargingTb pa = new ParkChargingTb();
+        pa.setIsOpen(isOpen);
+        pa.setId(id);
+        int delete = commonDao.updateByPrimaryKey(pa);
+        if(delete==1){
+            //通知泊链更新状态
+
+
+
+
+            result.put("state",1);
+            result.put("msg","更新失败！");
+        }
+        return result;
+    }
+
+    @Override
+    public JSONObject cloudFirst(Long comid, Integer cloudFirst) {
+        JSONObject result = new JSONObject();
+
+        return null;
+    }
+
+}
